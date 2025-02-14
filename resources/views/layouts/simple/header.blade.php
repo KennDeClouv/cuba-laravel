40342e01104e9f39e0d1e8b06be6e2a4f04cbbe6
KennDeClouv <146813701+KennDeClouv@users.noreply.github.com>
2025-02-14 07:29:03 +0700

    Clean up compiled view files and remove unnecessary header content
    - Remove compiled view files from storage/framework/views
    - Remove redundant notification and marketing content from header
    - Update .gitignore to include compiled view files

--- resources/views/layouts/simple/header.blade.php
+++ resources/views/layouts/simple/header.blade.php
@@ -15,27 +15,6 @@
                         </div>
                     </div>
                 </form>
-                <div class="header-logo-wrapper col-auto p-0">
-                    <div class="logo-wrapper"><a href="{{ route('admin.default_dashboard') }}"><img class="img-fluid for-light"
-                                src="{{ asset('assets/images/logo/logo.png') }}" alt=""><img
-                                class="img-fluid for-dark" src="{{ asset('assets/images/logo/logo_dark.png') }}"
-                                alt=""></a></div>
-                    <div class="toggle-sidebar"><i class="status_toggle middle sidebar-toggle"
-                            data-feather="align-center"></i></div>
-                </div>
-                <div class="left-header col-xxl-5 col-xl-6 col-lg-5 col-md-4 col-sm-3 p-0">
-                    <div class="notification-slider">
-                        <div class="d-flex h-100"> <img src="{{ asset('assets/images/giftools.gif') }}" alt="gif">
-                            <h6 class="mb-0 f-w-400"><span class="font-primary">Don't Miss Out! </span><span
-                                    class="f-light"> Out new update has been release.</span></h6><i
-                                class="icon-arrow-top-right f-light"></i>
-                        </div>
-                        <div class="d-flex h-100"><img src="{{ asset('assets/images/giftools.gif') }}" alt="gif">
-                            <h6 class="mb-0 f-w-400"><span class="f-light">Something you love is now on sale! </span>
-                            </h6><a class="ms-1" href="https://1.envato.market/3GVzd" target="_blank">Buy now !</a>
-                        </div>
-                    </div>
-                </div>
                 <div class="nav-right col-xxl-7 col-xl-6 col-md-7 col-8 pull-right right-header p-0 ms-auto">
                     <ul class="nav-menus">
                         <li class="language-nav">
